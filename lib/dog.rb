c82a74a7341bcc027e77793be10f98c17f47f1cb
Araba Taylor <araba.taylor20@gmail.com>
2018-02-09 18:47:26 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -54,9 +54,11 @@ class Dog
   end
 
   def self.find_by_name(name)
-    
+    sql = <<-SQL
+      SELECT * FROM dogs where name = ?
+    SQL
   end
-  
+
   def self.find_by_id(id)
     sql = <<-SQL
       SELECT * FROM dogs WHERE id = ?
