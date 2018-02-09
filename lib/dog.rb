8e70e953cce033e12467f9cb4f3d9829db892cd1
Araba Taylor <araba.taylor20@gmail.com>
2018-02-09 18:42:04 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -68,6 +68,7 @@ class Dog
     dog = DB[:conn].execute("SELECT * FROM dogs WHERE name = ? AND breed = ?", name, breed)
     if !dog.empty?
       dog_data = dog[0]
+      binding.pry
       dog = Dog.new(dog_data[0], dog_data[1], id:nil)
     else
       song = self.create(name:name, breed:breed)
